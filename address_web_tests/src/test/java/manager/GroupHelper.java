c6dd594dad60dc52c8508ab4ddd6c000351c7c11
mishin <maximmishin90@gmail.com>
2024-08-12 20:19:01 +0300

    Задание №6: Реализовать двухуровневую архитектуру тестового набора
    Сделать общий базовый класс для тестов TestBase
    Сделать класс ApplicationManager
    Выделить из ApplicationManager специализированные классы-помощники GroupHelper и LoginHelper
    Создать для этих вспомогательных классов общий базовый класс HelperBase

---
+++ address_web_tests/src/test/java/manager/GroupHelper.java
@@ -0,0 +1,84 @@
+package manager;
+
+import model.GroupData;
+import org.openqa.selenium.By;
+
+public class GroupHelper extends HelperBase {
+
+    public GroupHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+    public void createGroup(GroupData group) {
+        openGroupPage();
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupsPage();
+    }
+
+    public void removeGroup() {
+        openGroupPage();
+        selectGroup();
+        removeSelectedGroup();
+        returnToGroupsPage();
+    }
+
+    public void modifyGroup(GroupData modifiedGroup) {
+        openGroupPage();
+        selectGroup();
+        initGroupModification();
+        fillGroupForm(modifiedGroup);
+        submitGroupModification();
+        returnToGroupsPage();
+    }
+
+    public void openGroupPage() {
+        if (!manager.isElementPresent(By.name("new"))) {
+            click(By.linkText("groups"));
+        }
+    }
+
+    public boolean isGroupPresent() {
+        openGroupPage();
+        return manager.isElementPresent(By.name("selected[]"));
+    }
+
+    private void removeSelectedGroup() {
+        click(By.name("delete"));
+    }
+
+
+
+    private void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
+    private void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+    private void returnToGroupsPage() {
+        click(By.linkText("group page"));
+    }
+
+    private void submitGroupModification() {
+        click(By.name("update"));
+    }
+
+    private void fillGroupForm(GroupData group) {
+        type(By.name("group_name"), group.name());
+        type(By.name("group_header"), group.header());
+        type(By.name("group_footer"), group.footer());
+    }
+
+
+    private void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    private void selectGroup() {
+        click(By.name("selected[]"));
+    }
+
+}
---
+++ address_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    void canModifyGroup() {
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("Group name", "Group header", "Group footer"));
+        }
+        app.groups().modifyGroup(new GroupData().withName("modify name").withHeader("modify header").withFooter("modify footer"));
+    }
+}
