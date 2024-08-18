1e99d1b4e293c227d496a231e98f0856117d9932
mishin <maximmishin90@gmail.com>
2024-08-18 19:44:36 +0300

    optimize imports

--- address_web_tests/src/test/java/tests/ContactModificationTests.java
+++ address_web_tests/src/test/java/tests/ContactModificationTests.java
@@ -1,7 +1,6 @@
 package tests;
 
 import model.ContactData;
-import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
--- address_web_tests/src/test/java/tests/GroupModificationTests.java
+++ address_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -1,6 +1,5 @@
 package tests;
 
-import model.ContactData;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
